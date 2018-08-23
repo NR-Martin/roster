dfd119f1504d27ff08a9b864559a193babc087f9
Nick Martin <nicholas.martin@cgi.com>
2018-08-23 13:38:45 -0600

    more config changes

--- src/main/java/io/rscale/training/DisableBasicAuthAdapter.java
+++ src/main/java/io/rscale/training/DisableBasicAuthAdapter.java
@@ -1,9 +1,12 @@
+package io.rscale.training;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableDiscoveryClient
 @Configuration
 @EnableWebSecurity
 public class DisableBasicAuthAdapter extends WebSecurityConfigurerAdapter {
